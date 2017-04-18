b7b81301481a80359cc5d7c53b59bd1aef3eb9bc
Marcos Germano <mikazzz_pro@hotmail.com>
2017-04-18 21:55:31 +0100

    Added some basic information about the user

--- src/UserInformation.jsx
+++ src/UserInformation.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const UserInformation = props => (
-  <div>
-    Display the user information here
-  </div>
+import AppStyles from './App.css'
+
+const UserInformation = ({avatar, name, url, bio, location}) => (
+	<div className={AppStyles.User} >
+        <div className={AppStyles.UserAvatar}>
+          <img src={avatar} alt={name} />
+        </div>
+        <h1>{name}</h1>
+		<p>{url}</p>
+        <p>{bio}</p>
+        <p>{location}</p>
+	</div>
 );
 
 export default UserInformation;
+
